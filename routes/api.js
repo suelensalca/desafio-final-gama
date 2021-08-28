c7baa6dc8b1c9362e71015545b1c5586e3db86d6
suelensalsa <suelen.leaf@gmail.com>
2021-08-28 13:36:09 -0300

    api update

--- routes/api.js
+++ routes/api.js
@@ -2,22 +2,44 @@ var express = require("express");
 var router = express.Router();
 var db = require("../database");
 
-/* GET users listing. */
 router.get("/", function (req, res, next) {
   res.send("api response");
 });
 
-router.get("/teste", async function (req, res, next) {
-  let sqlRes = {};
+router.get("/candidatos", async function (req, res, next) {
+  let result = {};
 
   try {
-    sqlRes = await db.query("SELECT nome, email FROM candidatos")
+    result = await db.query("SELECT nome, email, cpf FROM candidatos")
   } catch (error) {
     console.log(error);
   } 
 
-  res.send(sqlRes.rows);
+  res.send(result.rows);
 
 });
+
+// inserir-candidato
+router.post("/inserir-candidato", async function (req, res, next) {
+  const result = {
+    status: 200,
+    data: {}
+  };
+
+  let body = req.body;
+  let queryStr = `INSERT INTO candidatos (${Object.keys(body).join(', ')}) VALUES (${Object.values(body).map((value) => `'${value}'` ).join(', ')})`
+
+  try {
+    await db.query(queryStr)
+    result.data = body
+  } catch (error) {
+    result.status = 500;
+    result.data = error;
+  } 
+
+  res.status(result.status).send(result);
+
+});
+
 
 module.exports = router;
